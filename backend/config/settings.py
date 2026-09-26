877db2727c9b92ca55825b182031efc0668b0a92
eugeniuss7 <eugenevillegas47@gmail.com>
2026-09-26 23:03:59 +0800

    basic configurations and structure

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,9 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,3 +128,8 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
---
+++ frontend/src/pages/Landing.jsx
@@ -0,0 +1,9 @@
+
+export default function Landing() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-6xl font-bold">Welcome to Kisig Kids</h1>
+      <p className="mt-3 text-2xl">Your one-stop solution for kids' needs!</p>
+    </div>
+  );
+}
